0feeb84034c955fbdafd58143f3559dfe79e5e7b
lima-wendel <wchristofer@gmail.com>
2021-07-13 09:23:34 -0300

    aplicando boas práticas com operador tenário

--- src/components/App4.js
+++ src/components/App4.js
@@ -19,16 +19,26 @@ class App4 extends React.Component {
       this,this.setState({nome: this.state.txtNome});
   }
   render() {
-    if(!this.state.nome) {
+    //if(!this.state.nome) {
+    const renderForm = ()=>{
         return (
             <>
             Nome: <input type='text' onChange={this.changeTxtNome}/>
             <button onClick={this.persistName}>Salvar</button>
             </>
-          )
-    } else {
-        return <p>Olá {this.state.nome}</p>
+        );
     }
+
+   // } else {
+    const renderText = ()=>{
+        return (
+        <>
+        <p>Olá {this.state.nome}</p>
+        </>
+        );
+    }
+    return !this.state.nome ? renderForm() : renderText();   
+    
   }
 }
 
